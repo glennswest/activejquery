95cbeed9740a6f10fa7a1f0a6ec6bee9088c6749
glennswest <glennswest@yahoo.com.sg>
2009-05-15 13:13:06 +0800

    Version .014

--- install.rb
+++ install.rb
@@ -4,11 +4,13 @@ RAILS_ROOT = File.dirname(__FILE__) + "/../../../" unless defined? RAILS_ROOT
 def copy_files(source_path, destination_path, directory)
   source, destination = File.join(directory, source_path), File.join(RAILS_ROOT, destination_path)
   FileUtils.mkdir(destination) unless File.exist?(destination)
-  FileUtils.cp(Dir.glob(source+'/*'), destination)
+  FileUtils.cp_r(Dir.glob(source+'/*'), destination, 
+                 {:remove_destination => true, :verbose => true})
 end
 
-directory = File.join(File.dirname(__FILE__), "copy_on_install")
+directory = File.dirname(__FILE__)
 copy_files("/public/css", "/public/css", directory)
 copy_files("/public/javascripts", "/public/javascripts", directory)
+copy_files("/app/viewsw/activejquery", "/app/views/activejquery", directory)
 
 
