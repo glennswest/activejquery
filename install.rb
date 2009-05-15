faf46c1d0ffb66ec8e3ede6547859d0a0905353b
glennswest <glennswest@yahoo.com.sg>
2009-05-15 11:36:13 +0800

    Version .012

--- install.rb
+++ install.rb
@@ -1,1 +1,14 @@
-# Install hook code here
+require 'fileutils'
+RAILS_ROOT = File.dirname(__FILE__) + "/../../../" unless defined? RAILS_ROOT
+ 
+def copy_files(source_path, destination_path, directory)
+  source, destination = File.join(directory, source_path), File.join(RAILS_ROOT, destination_path)
+  FileUtils.mkdir(destination) unless File.exist?(destination)
+  FileUtils.cp(Dir.glob(source+'/*'), destination)
+end
+
+directory = File.join(File.dirname(__FILE__), "copy_on_install")
+copy_files("/public/css", "/public/css", directory)
+copy_files("/public/javascripts", "/public/javascripts", directory)
+
+
